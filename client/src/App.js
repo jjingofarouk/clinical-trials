152045c35336f5b517ad0bb13b408906e585a68e
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-19 07:31:44 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import ClinicalTrials from './components/trials/ClinicalTrials';
+import TrialDetailPage from './components/trials/TrialDetailPage';
 import About from './components/About';
 import Contact from './components/Contact';
 import Privacy from './components/Privacy';
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/trials" element={<ClinicalTrials />} />
+            <Route path="/trials/:nctId" element={<TrialDetailPage />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/privacy" element={<Privacy />} />
