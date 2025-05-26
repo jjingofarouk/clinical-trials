6a760a58834917aa8de72aaff304acf0a7f451b2
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-26 18:32:23 +0300

    Update ForResearchers.js

--- client/src/components/ForResearchers.js
+++ client/src/components/ForResearchers.js
@@ -7,7 +7,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './ForResearchers.css';
+import './ForReseachers.css';
 
 const ForResearchers = () => {
   const navigate = useNavigate();
